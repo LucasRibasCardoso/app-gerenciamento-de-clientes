dc30a4c1090ec81fb8ac633561c6516c0ecafa7a
Lucas Cardoso <lucas.rib.card@gmail.com>
2025-02-02 13:40:28 -0300

    adicionado mensagem de erro caso não seja encontrado clientes na pesquisa

--- frontend/src/shared/components/tables/ClientTable.tsx
+++ frontend/src/shared/components/tables/ClientTable.tsx
@@ -52,111 +52,120 @@ const ClientsTable: React.FC<Props> = ({ clients, onSelectClient }) => {
         </TableHead>
         
         <TableBody>
-          {clients.map((client) => (
-            <React.Fragment key={client.id}>
-              {/* Linha Principal */}
-              <TableRow>
-                <TableCell width={"75px"}>
-                  <Checkbox  
-                    checked={selectedClient === client.id.toString()}
-                    onChange={() => handleCheckboxChange(client.id)}/>
-                </TableCell>
-                <TableCell width={"75px"}>
-                  <IconButton
-                    onClick={() => toggleRow(client.id)}
-                    aria-label="expand row"
-                    size="small"
-                  >
-                    {openRows[client.id] ? (
-                      <KeyboardArrowUp />
-                    ) : (
-                      <KeyboardArrowDown />
-                    )}
-                  </IconButton>
-                </TableCell>
-                <TableCell>{client.completeName}</TableCell>
-                <TableCell>{client.cpf || "-"}</TableCell>
-                <TableCell>{client.phone || "-"}</TableCell>
-                <TableCell>{client.email || "-"}</TableCell>
-                <TableCell>{client.birthDate || "-"}</TableCell>
-              </TableRow>
+          {clients.length === 0 ? (
+            <TableRow>
+              <TableCell 
+                colSpan={7} 
+                sx={{
+                  height: "45px",
+                  textAlign: "center",
+                  fontSize: "18px",
+                  fontWeight: "bold",
+                  color: "error.main"
+              }}>
+                Nenhum cliente encontrado.
+              </TableCell>
+            </TableRow>
+          ) : (
+            clients.map((client) => (
+              <React.Fragment key={client.id}>
+                {/* Linha Principal */}
+                <TableRow>
+                  <TableCell width={"75px"}>
+                    <Checkbox  
+                      checked={selectedClient === client.id.toString()}
+                      onChange={() => handleCheckboxChange(client.id)}
+                    />
+                  </TableCell>
+                  <TableCell width={"75px"}>
+                    <IconButton
+                      onClick={() => toggleRow(client.id)}
+                      aria-label="expand row"
+                      size="small"
+                    >
+                      {openRows[client.id] ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                    </IconButton>
+                  </TableCell>
+                  <TableCell>{client.completeName}</TableCell>
+                  <TableCell>{client.cpf || "-"}</TableCell>
+                  <TableCell>{client.phone || "-"}</TableCell>
+                  <TableCell>{client.email || "-"}</TableCell>
+                  <TableCell>{client.birthDate || "-"}</TableCell>
+                </TableRow>
 
-              {/* Linha Expandida */}
-              <TableRow>
-                <TableCell sx={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                  <Collapse in={openRows[client.id]} timeout="auto" unmountOnExit>
-                    <Box margin={1} display={"flex"} gap={"150px"}>
-                      {/* Endereço */}
-                      <Box>
-                        <Table size="small" aria-label="address">
-                          <Typography variant="subtitle1">
-                            Endereço
-                          </Typography>
-                          <TableBody>
-                            <TableRow>
-                              <TableCell>CEP</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.zipCode || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>País</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.country || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Estado</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.state || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Cidade</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.city || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Bairro</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.neighborhood || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Rua</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.street || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Complemento</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.address?.complement || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Número Residencial</TableCell>
-                              <TableCell sx={{ textAlign: "end" }}> {client.address?.residentialNumber || "-"}</TableCell>
-                            </TableRow>
-                          </TableBody>
-                        </Table>
+                {/* Linha Expandida */}
+                <TableRow>
+                  <TableCell sx={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+                    <Collapse in={openRows[client.id]} timeout="auto" unmountOnExit>
+                      <Box margin={1} display={"flex"} gap={"150px"}>
+                        {/* Endereço */}
+                        <Box>
+                          <Table size="small" aria-label="address">
+                            <Typography variant="subtitle1">Endereço</Typography>
+                            <TableBody>
+                              <TableRow>
+                                <TableCell>CEP</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.zipCode || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>País</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.country || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Estado</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.state || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Cidade</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.city || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Bairro</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.neighborhood || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Rua</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.street || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Complemento</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.complement || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Número Residencial</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.address?.residentialNumber || "-"}</TableCell>
+                              </TableRow>
+                            </TableBody>
+                          </Table>
+                        </Box>
+
+                        {/* Passaporte */}
+                        <Box>
+                          <Table size="small" aria-label="passport">
+                            <Typography variant="subtitle1">Passaporte</Typography>
+                            <TableBody>
+                              <TableRow>
+                                <TableCell>Número</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.passport?.number ?? "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Data de Emissão</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.passport?.emissionDate || "-"}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Data de Vencimento</TableCell>
+                                <TableCell sx={{ textAlign: "end" }}>{client.passport?.expirationDate || "-"}</TableCell>
+                              </TableRow>
+                            </TableBody>
+                          </Table>
+                        </Box>
                       </Box>
-                      
-                      {/* Passaporte */}
-                      <Box>
-                        <Table size="small" aria-label="passport">
-                          <Typography variant="subtitle1">
-                            Passaporte
-                          </Typography>
-                          <TableBody>
-                            <TableRow>
-                              <TableCell>Número</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.passport?.number ?? "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Data de Emissão</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.passport?.emissionDate || "-"}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Data de Vencimento</TableCell>
-                              <TableCell sx={{ textAlign: "end" }} >{client.passport?.expirationDate || "-"}</TableCell>
-                            </TableRow>
-                          </TableBody>
-                        </Table>
-                      </Box>
-                    </Box>
-                  </Collapse>
-                </TableCell>
-              </TableRow>
-            </React.Fragment>
-          ))}
+                    </Collapse>
+                  </TableCell>
+                </TableRow>
+              </React.Fragment>
+            ))
+          )}
         </TableBody>
       </Table>
     </TableContainer>
