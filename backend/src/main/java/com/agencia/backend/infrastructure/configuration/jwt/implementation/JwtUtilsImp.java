2513a55aa6a4af39d1f795f477fac1dc6ea45cc5
Lucas Cardoso <lucas.rib.card@gmail.com>
2025-03-15 20:40:49 -0300

    Alterada a variavel de ambiente APP_PROFILE

--- backend/src/main/java/com/agencia/backend/infrastructure/configuration/jwt/implementation/JwtUtilsImp.java
+++ backend/src/main/java/com/agencia/backend/infrastructure/configuration/jwt/implementation/JwtUtilsImp.java
@@ -15,7 +15,9 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtUtilsImp implements JwtUtils {
 
   @Value("${api.security.token.jwt}")
